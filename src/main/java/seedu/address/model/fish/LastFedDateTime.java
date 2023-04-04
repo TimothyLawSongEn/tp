7e8a45da8fd7f18572ff08096dcddeb3ea38fac0
TimothyLawSongEn <e0725564@u.nus.edu>
2023-04-04 13:42:24 +0800

    Attempt to fix DateTime validation

--- src/main/java/seedu/address/model/fish/LastFedDateTime.java
+++ src/main/java/seedu/address/model/fish/LastFedDateTime.java
@@ -3,8 +3,14 @@ package seedu.address.model.fish;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.Date;
 
 import seedu.address.model.date.DateUtil;
 
@@ -19,8 +25,10 @@ public class LastFedDateTime {
     public static final String MESSAGE_CONSTRAINTS_NOT_FUTURE =
             "Last Fed Date Time cannot be in the future.";
     public static final String VALIDATION_REGEX =
-            "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4} (?:[01]\\d|2[0-3]):[0-5]\\d$";
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+            "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/((?!00)[0-9]{2}(?!00)|[0-9]{4}) (?:[01][0-9]|2[0-3]):[0-5][0-9]$";
+
+//            "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4} (?:[01]\\d|2[0-3]):[0-5]\\d$";
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withResolverStyle(ResolverStyle.SMART);
 
     public final String value;
     public final LocalDateTime localDateTime;
@@ -38,6 +46,7 @@ public class LastFedDateTime {
         value = lastFedDateTime;
         localDateTime = LocalDateTime.parse(lastFedDateTime, formatter);
 
+
         checkArgument(isNotFuture(lastFedDateTime), MESSAGE_CONSTRAINTS_NOT_FUTURE);
 
         alphaNumericDateTime = DateUtil.getTaskDescriptionDateTimeFormat(localDateTime);
@@ -47,6 +56,25 @@ public class LastFedDateTime {
      * Returns true if a given string is a valid last Fed Date number.
      */
     public static boolean isValidDateTime(String test) {
+
+//        LocalDateTime dateTime = LocalDateTime.parse(test, formatter);
+//
+//        // Extract just the date (dd/MM/yyyy) from the LocalDateTime object
+//        LocalDate date = dateTime.toLocalDate();
+//
+//        // Format the date as a string in the format of "dd/MM/yyyy"
+//        String outputDate = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+//
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+//        dateFormat.setLenient(false);
+//
+//        try {
+//            Date datee = dateFormat.parse(outputDate);
+//            System.out.println("Valid date: " + date);
+//        } catch (ParseException e) {
+//            System.out.println("Invalid date: " + outputDate);
+//        }
+
         return test.matches(VALIDATION_REGEX);
     }
 
